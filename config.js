15de6660e36f69fd4141f811ed4ea4cb91fd58f0
paradopax <andrea.dipace.00@gmail.com>
2022-06-01 21:30:50 +0200

    Split index.js into index.js and app.js

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
-var mongoose = require('mongoose');
-require('dotenv').config();;
+'use strict';
 
+var mongoose = require('mongoose');
+require('dotenv').config();
 
 const DB_CONNECTION_STRING = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.5lue6.mongodb.net/noteapp?retryWrites=true&w=majority`;
 
-mongoose.connect(DB_CONNECTION_STRING, {
-	useNewUrlParser: true
-}, (err) => {
-    if (err) throw(new Error("DB can't connect"))
-    console.log("DB connected")
-});
-
+module.exports.initDB = () => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DB_CONNECTION_STRING, {
+            useNewUrlParser: true
+        }, (err) => {
+            if (err) reject("DB can't connect");
+            resolve("DB connected");
+        });
+    }) 
+} 
